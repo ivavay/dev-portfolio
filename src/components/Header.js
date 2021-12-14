0dbea816502ebc0a547d00ced4680fef88d74971
Ivy Chen <ichen9@ucsc.edu>
2021-12-13 18:04:13 -0800

    added responsive for large screens

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from "react"
 function Header() {
     return(
         <div>
-            <h1 className="animated animatedFadeInUp fadeInUp" >I’m Ivy Chen — a designer/developer <i>soon to be</i> in Taipei, Taiwan</h1>
+            <h1 className="home" className="animated animatedFadeInUp fadeInUp" >I’m Ivy Chen — a designer/developer <i>soon to be</i> in Taipei, Taiwan</h1>
             <div className="gradient"></div>
         </div>)
 }
